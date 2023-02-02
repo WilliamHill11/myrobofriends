55fcb49b0f40aac73714f14340160b2fc518addb
william hill <williamhill0831@Hotmail.com>
2023-02-01 23:21:53 -0500

    changed wording on failed loading

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -14,7 +14,7 @@ class ErrorBoundry extends Component {
 
     render() {
         if (this.state.hasError) {
-            return <h1>Oooopppps. This is not good & idk what is you should do</h1>
+            return <h1>Oooopppps. This is not good</h1>
         }
         return this.props.children
     }
